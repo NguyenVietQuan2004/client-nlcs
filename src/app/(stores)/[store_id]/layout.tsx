7ad86cbcfe0ed23f995b65e9a3e86679708281fa
Nguyenvietquan2004 <nguyenvietquan.30032004@gmail.com>
2025-03-15 16:45:10 +0700

    footer

--- src/app/(stores)/[store_id]/layout.tsx
+++ src/app/(stores)/[store_id]/layout.tsx
@@ -1,6 +1,6 @@
-// import Footer from "@/components/footer";
 import Wrapper from "@/components/wrapper";
 import MainNavbar from "@/app/(stores)/[store_id]/_navbar/main-nav";
+import Footer from "@/components/footer";
 
 function StoreLayout({
   children,
@@ -11,7 +11,7 @@ function StoreLayout({
     <div>
       <MainNavbar />
       <Wrapper>{children}</Wrapper>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
